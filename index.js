235efd86f81252fb45782b7e39ed21ac09802ccf
Samuel Mills <spam.mills@hotmail.com>
2014-10-14 11:17:49 +0200

    v0.0.10 Added IP location for support. Improved Logger.output for callback support

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ var colourSettings = require('./lib/colour.js');
 
 //Settings
 var pack = require('./package.json');
+var ipify = require('ipify');
+var IP = "127.0.0.1";
+
 
 //Core logger function
 var Logger = function(severity, source, message, location, timestamp) {
@@ -20,8 +23,16 @@ var Logger = function(severity, source, message, location, timestamp) {
 			if (typeof severity !== "undefined" && typeof source !== "undefined" && typeof message !== "undefined") {
 				if (typeof location === "undefined") {
 					if (Logger.core.settings.location === "undefined"){
-						Logger.core.settings.location = os.hostname();
-						location = os.hostname();
+						if (Logger.core.settings.defaultLocation === "ip") {
+							Logger.core.settings.location = IP;
+							location = IP;
+						} else if (Logger.core.settings.defaultLocation === "hostname") {
+							Logger.core.settings.location = os.hostname();
+							location = os.hostname();
+						} else {
+							Logger.core.settings.location = os.hostname();
+							location = os.hostname();
+						}
 					} else {
 						location = Logger.core.settings.location;
 					} if (typeof timestamp === "undefined") {
@@ -56,7 +67,7 @@ var Logger = function(severity, source, message, location, timestamp) {
 			}, 100);
 		}
 	} else {
-		Logger("warning", "Logger", "An object was passed to Jethro, support for this is currently unavailable!")
+		Logger("warning", "Logger", "An object was passed to Jethro, support for this is currently unavailable!");
 	}
 };
 
@@ -65,6 +76,7 @@ Logger.core = {
 	initialised: false,
 
 	settings: {
+		defaultLocation: "ip",
 		location: "undefined",
 		timeformat: "undefined",
 		output: {
@@ -93,7 +105,7 @@ Logger.core = {
 	init: function(options) {
 
 		if (typeof options === "undefined") {
-			options = {}
+			options = {};
 		}
 		if (Logger.core.initialised === false) {
 
@@ -101,26 +113,26 @@ Logger.core = {
 			Logger.core.initialised = true;
 
 			//Sets the loggers settings
-			Logger.settings.set(options)
+			Logger.settings.set(options);
 
 			//Startup the respective services...
 			if (typeof options.file !== "undefined") {
 				if (options.file.run === true) {
-					Logger.startFile(options.file)
+					Logger.startFile(options.file);
 				}
 			} 
 			if (typeof options.socket !== "undefined") {
 				if (options.socket.run === true) {
-					Logger.startSocket(options.socket)
+					Logger.startSocket(options.socket);
 				}
 			} 
 			if (typeof options.database !== "undefined") {
 				if (options.database.run === true) {
-					Logger.startDatabase(options.database)
+					Logger.startDatabase(options.database);
 				}
 			}
 			if (process.argv.indexOf('-output')>-1){
-				var i = process.argv.indexOf('-output')
+				var i = process.argv.indexOf('-output');
 				var j = i+1;
 				if (process.argv[j] === 'false') {
 					Logger.core.settings.output.console = false;
@@ -130,8 +142,13 @@ Logger.core = {
 			} else {
 				Logger.core.settings.output.console = true;
 			}
+			if (options.defaultLocation === "ip") {
+				Logger.core.settings.location = IP;
+			} else if (options.defaultLocation === "hostname") {
+				Logger.core.settings.location = os.hostname();
+			}
 			if (options.quickStart !== true) {
-				Logger.output({timestamp:new Date(), message:"Logger "+pack.version+" succesfully initialised!", source:"Logger", severity:"success"})
+				Logger.output({timestamp:new Date(), message:"Logger "+pack.version+" succesfully initialised!", source:"Logger", severity:"success"});
 			}
 		} else {
 			
@@ -153,16 +170,19 @@ Logger.settings = {
 			Logger.core.settings[prop] = options[prop];
 		}
 
-		catchUncaught()
-		catchExit()
+		catchUncaught();
+		catchExit();
 	},
 	addToWhiteList: function(parameter, arr) {
 
 	},
 	addToBlackList: function(parameter, arr) {
 
+	},
+	setLocation: function(loc) {
+		Logger.core.settings.location = loc;
 	}
-}
+};
 
 Logger.init = function(options) {	
 	Logger.core.init(options);
@@ -180,7 +200,10 @@ Logger.emitter = new EventEmitter({
 });
 
 Logger.emitter.on('logger', function(data){
-	Logger.output(data)
+	Logger.output(data);
+	/*Logger.output(data, function(log){
+		console.log(log);
+	});*/
 });
 
 Logger.output = function(data, callback) {
@@ -192,55 +215,78 @@ Logger.output = function(data, callback) {
 			var d = "";
 			var e = "";
 			var f = "";
-			if (typeof data.timestamp !== "undefined" && Logger.core.settings.output.displayOpts.timestamp === true) {
-				if (Logger.core.settings.timeformat !== "undefined" && Logger.core.settings.timeformat !== "undefined") {
-					try {
-						f = moment().format(Logger.core.settings.timeformat);
-					} catch (e) {
-						f = moment().format('DD MMM HH:mm:ss');
+			if (Logger.core.settings.output.displayOpts.timestamp === true || typeof callback === "function") {
+				if (typeof data.timestamp !== "undefined"){
+					if (Logger.core.settings.timeformat !== "undefined" && Logger.core.settings.timeformat !== "undefined") {
+						try {
+							f = moment().format(Logger.core.settings.timeformat);
+						} catch (e) {
+							f = moment().format('DD MMM HH:mm:ss');
+						}
+					} else {
+						f = Logger.util.formatTimestamp(data.timestamp);
 					}
 				} else {
-					f = Logger.util.formatTimestamp(data.timestamp);
+					f = Logger.util.formatTimestamp(new Date());
 				}
 			} if (Logger.core.settings.output.timestampOpts.brackets === true) {
 				a = "["+f+"] ";
 			} else {
 				a = f+" ";
-			} if (typeof data.severity !== "undefined" && Logger.core.settings.output.displayOpts.severity === true) {
-				var h = Logger.util.capitiliseFirstLetter(data.severity);
-				switch (data.severity.toLowerCase()) {
-		            case 'success':
-		            	b = "[" + h.success + "]   ";
-		            break;
-		            case 'transport':
-		            	b = "[" + h.transport + "] ";
-		            break;
-		            case 'debug':
-		            	b = "[" + h.debug + "]     ";
-		            break;
-		            case 'info':
-		            	b = "[" + h.info + "]      ";
-		            break;
-		            case 'warning':
-		            	b = "[" + h.warning + "]   ";
-		            break;
-		            case 'error':
-		            	b = "[" + h.error + "]     ";
-		            break;
-		            default:
-						b = "[" + h + "]     ";
-		        }
-			} if (typeof data.location !== "undefined" && Logger.core.settings.output.displayOpts.location === true) {
-				c = "[" + data.location + "] ";
-			} if (typeof data.source !== "undefined" && Logger.core.settings.output.displayOpts.source === true) {
-				d = "[" + data.source + "] ";
-			} if (typeof data.message !== "undefined" && Logger.core.settings.output.displayOpts.message === true) {
-				e = "" + data.message + "";
+			} if (Logger.core.settings.output.displayOpts.severity === true || typeof callback === "function") {
+				if (typeof data.severity !== "undefined"){
+					var h = Logger.util.capitiliseFirstLetter(data.severity);
+					switch (data.severity.toLowerCase()) {
+			            case 'success':
+			            	b = "[" + h.success + "]   ";
+			            break;
+			            case 'transport':
+			            	b = "[" + h.transport + "] ";
+			            break;
+			            case 'debug':
+			            	b = "[" + h.debug + "]     ";
+			            break;
+			            case 'info':
+			            	b = "[" + h.info + "]      ";
+			            break;
+			            case 'warning':
+			            	b = "[" + h.warning + "]   ";
+			            break;
+			            case 'error':
+			            	b = "[" + h.error + "]     ";
+			            break;
+			            default:
+							b = "[" + h + "]     ";
+			        }
+				} else {
+					b = "[" + "undefined".error + "]      ";
+				}
+			} if (Logger.core.settings.output.displayOpts.location === true || typeof callback === "function") {
+				if (typeof data.location !== "undefined"){
+					c = "[" + data.location + "] 	";
+				} else {
+					c = "[" + Logger.core.settings.location + "] 	";
+				}
+			} if (Logger.core.settings.output.displayOpts.source === true || typeof callback === "function") {
+				if (typeof data.source !== "undefined"){
+					d = "[" + data.source + "] ";
+				} else {
+					d = "[" + "undefined" + "] ";
+				}
+			} if (Logger.core.settings.output.displayOpts.message === true || typeof callback === "function") {
+				if (typeof data.message !== "undefined"){
+					e = "" + data.message + "";
+				} else {
+					e = "" + "undefined".error + "";
+				}
 			}
-			var output = (a + b + c + d + "	" + e + "	")
+			var output = (a + b + c + d + "	" + e + "	");
 
+			if (typeof callback !== "undefined") {
+				callback(output);
+			} else {
 				console.log(output);
-			
+			}
 		} else {
 			throw new Error("A non-object was sent to the Logger.output() function! See: "+util.inspect(data));
 		}
@@ -269,9 +315,9 @@ var catchUncaught = function() {
 	if (Logger.core.settings.catchUncaught === true) {
 		process.on('uncaughtException', function(e) {
 		    logger('error', 'Exception', 'Logger caught exception: '+e.stack);
-		})
+		});
 	}
-}
+};
 
 var catchExit = function() {
 	if (Logger.core.settings.catchExit === true) {
@@ -279,7 +325,7 @@ var catchExit = function() {
 	    	logger('error', 'Caught Exit', 'Logger caught exit with code: ' + e);
 		});
 	}
-}
+};
 
 //------------------------------- File writer -----------------------------------
 Logger.startFile = function(options) {
@@ -288,5 +334,14 @@ Logger.startFile = function(options) {
 	Logger("warning", "Logger", "The File logging utility is not complete and as a result will not load!");
 };
 //Place holder for the write to file module!
+
+ipify(function (err, ip) {
+	IP = ip;
+	if (Logger.core.settings.defaultLocation === "ip") {
+		if (Logger.core.settings.location !== "127.0.0.1" || Logger.core.settings.location !== "undefined") {
+			Logger.core.settings.location = ip;
+		}
+	}
+});
 
 module.exports = Logger;
